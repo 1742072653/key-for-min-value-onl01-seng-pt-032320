9e4f71057b5d047a75cb1854dd98d3de934e9a62
Wenhao Guo <gwh1742072653@gmail.com>
2020-03-30 17:40:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,15 +4,5 @@ def key_for_min_value(hash)
   if (hash == nil) 
     return nil
   else
-    hash.each do |key,value|
-      if (num == nil)
-        name = key
-        num = value
-      elsif  (num > value)
-        name = key
-        num = value
-      end
-    end
-    return name
-  end
+  return name
 end
